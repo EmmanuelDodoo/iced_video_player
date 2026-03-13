69b49371e6faec5249478156aed430bf6d4edee8
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2026-03-13 13:42:08 -0400

    nix: export RUST_SRC_PATH

--- crates.nix
+++ crates.nix
@@ -7,6 +7,12 @@
   }: let
     gstreamer-plugins = with pkgs.gst_all_1; [gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly];
   in {
+    nci.toolchainConfig = {
+      channel = "stable";
+      profile = "default";
+      components = ["rust-src"];
+    };
+
     # declare projects
     nci.projects."iced_video_player" = {
       path = ./.;
@@ -27,8 +33,11 @@
         buildInputs = with pkgs; [pkg-config glib libxkbcommon] ++ gstreamer-plugins;
       };
 
-      drvConfig.env = {
-        "GST_PLUGIN_PATH" = lib.makeLibraryPath gstreamer-plugins;
+      drvConfig.env = let
+        rustToolchain = config.nci.toolchains.mkShell pkgs;
+      in {
+        GST_PLUGIN_PATH = lib.makeLibraryPath gstreamer-plugins;
+        RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/src";
       };
     };
     # configure crates
